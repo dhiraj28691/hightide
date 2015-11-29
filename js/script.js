439bbc2e5d77c99ddce6fc4d57dce1dc8414670f
sashakluchnik <me@sashakluchnik.com>
2015-11-29 18:54:31 -0500

    ios chrome scroll removed

--- js/script.js
+++ js/script.js
@@ -9,6 +9,10 @@ window.requestAnimFrame = (function(){
 		  };
 })();
 
+window.onbeforeunload = function(){
+	window.scrollTo(0,0);
+}
+
 var images;
 var $window;
 var $body;
@@ -18,161 +22,181 @@ var offScroll;
 
 
 
-$(document).ready(function() {
-
-	images = new Array;
-	$window = $( window );
-	$body = $( 'body' );
+$window = $( window );
+$body = $( 'body' );
+images = new Array;
 
 
-	$('#loader .container').fadeIn(1000);
+$('#loader .container').fadeIn(1000);
 
-	preloadImages();
+var windowHeight = window.screen.availHeight;
+
+$('#video').css({
+	'top': windowHeight / 2,
+	'min-height': windowHeight
+});
+
+$('#overlay').css({
+	'height': windowHeight
+});
+
+if( navigator.userAgent.match('CriOS') ) {
+
+	var defaultPrevent=function(e){e.preventDefault();}
+	// document.body.parentElement.addEventListener("touchstart", defaultPrevent);
+	document.body.parentElement.addEventListener("touchmove" , defaultPrevent);
+	// document.body.addEventListener("touchstart", defaultPrevent);
+	document.body.addEventListener("touchmove" , defaultPrevent);
+
+	alert('Chrome iOS!');
+}
+
+
+preloadImages();
 
 
 
-	// loader
+// loader
 
-	var c = document.getElementById('c'),
-	    ctx = c.getContext('2d'),
-	    cw = c.width = window.innerWidth,
-	    ch = c.height = window.innerHeight,
-	    points = [],
-	    tick = 0,
-	    level = .1,
-	    opt = {
-	      count: 12,
-	      range: {
-	        x: 20,
-	        y: 80
-	      },
-	      duration: {
-	        min: 20,
-	        max: 40
-	      },
-	      level: 0.2,
-	      curved: true
-	    },
-	    rand = function(min, max){
-	        return Math.floor( (Math.random() * (max - min + 1) ) + min);
-	    },
-	    ease = function (t, b, c, d) {
-	      if ((t/=d/2) < 1) return c/2*t*t + b;
-	      return -c/2 * ((--t)*(t-2) - 1) + b;
-	    };
+var c = document.getElementById('c'),
+    ctx = c.getContext('2d'),
+    cw = c.width = window.innerWidth,
+    ch = c.height = window.innerHeight,
+    points = [],
+    tick = 0,
+    level = .1,
+    opt = {
+      count: 12,
+      range: {
+        x: 20,
+        y: 80
+      },
+      duration: {
+        min: 20,
+        max: 40
+      },
+      level: 0.2,
+      curved: true
+    },
+    rand = function(min, max){
+        return Math.floor( (Math.random() * (max - min + 1) ) + min);
+    },
+    ease = function (t, b, c, d) {
+      if ((t/=d/2) < 1) return c/2*t*t + b;
+      return -c/2 * ((--t)*(t-2) - 1) + b;
+    };
 
-	ctx.lineJoin = 'round';
+ctx.lineJoin = 'round';
 
-	var Point = function(config){
-	  this.anchorX = config.x;
-	  this.anchorY = config.y;
-	  this.x = config.x;
-	  this.y = config.y;
-	  this.setTarget();
-	};
+var Point = function(config){
+  this.anchorX = config.x;
+  this.anchorY = config.y;
+  this.x = config.x;
+  this.y = config.y;
+  this.setTarget();
+};
 
-	Point.prototype.setTarget = function(){
-	  this.initialX = this.x;
-	  this.initialY = this.y;
-	  this.targetX = this.anchorX + rand(0, opt.range.x * 2) - opt.range.x;
-	  this.targetY = this.anchorY + rand(0, opt.range.y * 2) - opt.range.y;
-	  this.tick = 0;
-	  this.duration = rand(opt.duration.min, opt.duration.max);
+Point.prototype.setTarget = function(){
+  this.initialX = this.x;
+  this.initialY = this.y;
+  this.targetX = this.anchorX + rand(0, opt.range.x * 2) - opt.range.x;
+  this.targetY = this.anchorY + rand(0, opt.range.y * 2) - opt.range.y;
+  this.tick = 0;
+  this.duration = rand(opt.duration.min, opt.duration.max);
+}
+
+Point.prototype.update = function(){
+  var dx = this.targetX - this.x;
+  var dy = this.targetY - this.y;
+  var dist = Math.sqrt(dx * dx + dy * dy);
+
+  if(Math.abs(dist) <= 0){
+    this.setTarget();
+  } else {
+    var t = this.tick;
+    var b = this.initialY;
+    var c = this.targetY - this.initialY;
+    var d = this.duration;
+    this.y = ease(t, b, c, d);
+
+    b = this.initialX;
+    c = this.targetX - this.initialX;
+    d = this.duration;
+    this.x = ease(t, b, c, d);
+
+    this.tick++;
+  }
+};
+
+Point.prototype.render = function(){
+  ctx.beginPath();
+  ctx.arc(this.x, this.y, 3, 0, Math.PI * 2, false);
+  ctx.fill();
+};
+
+
+var updatePoints = function(){
+	var i = points.length;
+	while(i--){
+		points[i].update();
+	}
+  	var i = opt.count + 2;
+	var spacing = (cw + (opt.range.x * 2)) / (opt.count-1);
+	while(i--){
+	  points.push(new Point({
+	    x: (spacing * (i - 1)) - opt.range.x,
+	    y: ch - (ch * level)
+	  }));
+	}
+};
+
+
+var renderShape = function(){
+  ctx.beginPath();
+  var pointCount = points.length;
+  ctx.moveTo(points[0].x, points[0].y);
+  var i;
+  for (i = 0; i < pointCount - 1; i++) {
+    var c = (points[i].x + points[i + 1].x) / 2;
+    var d = (points[i].y + points[i + 1].y) / 2;
+    ctx.quadraticCurveTo(points[i].x, points[i].y, c, d);
+  }
+  ctx.lineTo(-opt.range.x - opt.thickness, ch + opt.thickness);
+  ctx.lineTo(cw + opt.range.x + opt.thickness, ch + opt.thickness);
+  ctx.closePath();
+  ctx.fillStyle = '#034268';
+  ctx.fill();
+};
+
+
+var clear = function(){
+  ctx.clearRect(0, 0, cw, ch);
+};
+
+
+var level = 0;
+
+
+function loop() {
+
+	if(level < 1.9) {
+		window.requestAnimFrame(loop, c);
+		tick++;
+		clear();
+		updatePoints();
+		renderShape();
+		level = level + .004;
 	}
 
-	Point.prototype.update = function(){
-	  var dx = this.targetX - this.x;
-	  var dy = this.targetY - this.y;
-	  var dist = Math.sqrt(dx * dx + dy * dy);
+	if(level >= 1) {
+		loader.call();
+	}
 
-	  if(Math.abs(dist) <= 0){
-	    this.setTarget();
-	  } else {
-	    var t = this.tick;
-	    var b = this.initialY;
-	    var c = this.targetY - this.initialY;
-	    var d = this.duration;
-	    this.y = ease(t, b, c, d);
+};
 
-	    b = this.initialX;
-	    c = this.targetX - this.initialX;
-	    d = this.duration;
-	    this.x = ease(t, b, c, d);
-
-	    this.tick++;
-	  }
-	};
-
-	Point.prototype.render = function(){
-	  ctx.beginPath();
-	  ctx.arc(this.x, this.y, 3, 0, Math.PI * 2, false);
-	  ctx.fill();
-	};
+loop( loader );
 
 
-	var updatePoints = function(){
-		var i = points.length;
-		while(i--){
-			points[i].update();
-		}
-	  	var i = opt.count + 2;
-		var spacing = (cw + (opt.range.x * 2)) / (opt.count-1);
-		while(i--){
-		  points.push(new Point({
-		    x: (spacing * (i - 1)) - opt.range.x,
-		    y: ch - (ch * level)
-		  }));
-		}
-	};
-
-
-	var renderShape = function(){
-	  ctx.beginPath();
-	  var pointCount = points.length;
-	  ctx.moveTo(points[0].x, points[0].y);
-	  var i;
-	  for (i = 0; i < pointCount - 1; i++) {
-	    var c = (points[i].x + points[i + 1].x) / 2;
-	    var d = (points[i].y + points[i + 1].y) / 2;
-	    ctx.quadraticCurveTo(points[i].x, points[i].y, c, d);
-	  }
-	  ctx.lineTo(-opt.range.x - opt.thickness, ch + opt.thickness);
-	  ctx.lineTo(cw + opt.range.x + opt.thickness, ch + opt.thickness);
-	  ctx.closePath();
-	  ctx.fillStyle = '#034268';
-	  ctx.fill();
-	};
-
-
-	var clear = function(){
-	  ctx.clearRect(0, 0, cw, ch);
-	};
-
-
-	var level = 0;
-
-
-	function loop() {
-
-		if(level < 1.9) {
-			window.requestAnimFrame(loop, c);
-			tick++;
-			clear();
-			updatePoints();
-			renderShape();
-			level = level + .004;
-		}
-
-		if(level >= 1) {
-			loader.call();
-		}
-
-	};
-
-	loop( loader );
-	// loader();
-
-});
 
 
 
@@ -191,10 +215,12 @@ $(window).load(function() {
 		targetStep = Math.max( Math.round( offsetY / 10) , 1 );
 
 		if(targetStep != step ) {
-			step += ( targetStep - step ) / 20;
+			step += ( targetStep - step ) / 10;
 		}
 
 		changeFrame(step, images);
+
+		// requestAnimationFrame(animloop);
 	}
 
 
@@ -209,7 +235,6 @@ $(window).load(function() {
 
   maxScroll = $body.height() - 2 * window.innerHeight;
 
-	var scrollTrigger = false;
 
 
 	$window.on( 'scroll', function() {
@@ -221,34 +246,25 @@ $(window).load(function() {
 			animloop( offsetY, step, images );
 		});
 
-		console.log( 'maxScroll =', maxScroll );
-		console.log( 'offsetY =', offsetY );
-		console.log( '$body.height() =', $body.height() );
-		console.log( 'window.innerHeight =', window.innerHeight );
 
-		if ( offsetY < maxScroll ) {
+		// requestAnimationFrame( animloop( offsetY, step, images ) );
 
+		// console.log( 'maxScroll =', maxScroll );
+		// console.log( 'offsetY =', offsetY );
+		// console.log( '$body.height() =', $body.height() );
+		// console.log( 'window.innerHeight =', window.innerHeight );
+
+		if ( offsetY < maxScroll - window.innerHeight / 4 ) {
+			 console.log('<');
 			if( scrollTrigger == true ){
-				scrollTrigger = false;
-				console.log( 'scrollTrigger =', scrollTrigger );
-				$('#surface').fadeOut(800);
-				$('#godeep').delay(800).fadeIn(800);
-				console.log('<');
+				getUp();
 			}
-
 		}
 
-		if ( offsetY >= maxScroll ) {
-
-			if( scrollTrigger == false ){
-				scrollTrigger = true;
-				console.log( 'scrollTrigger =', scrollTrigger );
-
-				$body.scrollTo( maxScroll + 2 * window.innerHeight, { duration: 800 });
-				$('#vr').animate({ opacity: 1 }, 1500);
-				$('#surface').fadeIn(800);
-				$('#godeep').fadeOut(800);
+		if ( offsetY >= maxScroll + window.innerHeight / 4 ) {
 				console.log('>=');
+			if( scrollTrigger == false ){
+				getDown();
 			}
 		}
 
@@ -266,7 +282,6 @@ $(window).load(function() {
 	$('#surface').on( 'click', function (event) {
 		event.preventDefault();
 		$body.scrollTo( 0, { duration: 4000 });
-		$('#vr').animate({ opacity: 0 }, 800);
 	});
 
 
@@ -284,20 +299,40 @@ $(window).load(function() {
 		transform = Math.sin( n ) * Math.log( n ) * 21;
 		rotate = Math.cos( n ) * Math.log( n ) * -6 - 1;
 
-		$(el).css( '-webkit-transform', 'rotate('+rotate+'deg) translateX('+transform+'%)' );
-		$(el).css( 'transform', 'rotate('+rotate+'deg) translateX('+transform+'%)' );
+		$(el).css({
+			'-webkit-transform': 'rotate('+rotate+'deg) translateX('+transform+'%)',
+			'transform': 'rotate('+rotate+'deg) translateX('+transform+'%)'
+		});
 
 	});
 });
 
+var scrollTrigger = false;
+
+function getUp() {
+	scrollTrigger = false;
+	$('#surface').fadeOut(800);
+	$('#godeep').delay(800).fadeIn(800);
+	$('#vr').animate({ opacity: 0 }, 800);
+}
+
+function getDown() {
+	scrollTrigger = true;
+	$body.scrollTo( maxScroll + 2 * window.innerHeight, { duration: 800 });
+	$('#vr').animate({ opacity: 1 }, 1500);
+	$('#surface').fadeIn(800);
+	$('#godeep').fadeOut(800);
+}
+
 
 var loaded = false;
+
 
 function loader() {
 	if( loaded == false ){
 		$('#loader').delay(1000).fadeOut(2000);
-		$('#content').delay(4000).animate({ opacity: 1 }, 1500);
-		$('#godeep').delay(5000).fadeIn(800);
+		$('#content').delay(4000).animate({ opacity: 1 }, 1200);
+		$('#godeep').delay(5200).fadeIn(800);
 		$body.scrollTo( 8 * window.innerHeight, { duration: 0 } );
 		$body.delay(1400).scrollTo( 0, { duration: 2500 }, initAutoSurface);
 	}
@@ -321,6 +356,7 @@ function preloadImages() {
 }
 
 
+
 function initAutoSurface() {
 	$window.on('scroll', scrolled );
 }
@@ -332,7 +368,7 @@ function scrolled() {
 
 	offScroll = setTimeout( function() {
 
-		console.log( 'scrolled' );
+		// console.log( 'scrolled' );
 
     if ( offsetY < maxScroll ) {
 
